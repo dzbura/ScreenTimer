49174ef47c0044f7fe77f08fb3323f9ef11c9933
karzkowl <liliana.karzkowiak@contractors.roche.com>
2021-06-15 13:16:05 +0200

    String resources extracted, changed goal setting functionality to seekbar

--- ScreenTimer/app/src/main/java/com/example/screentimer/ui/SetGoalFragment.kt
+++ ScreenTimer/app/src/main/java/com/example/screentimer/ui/SetGoalFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -21,9 +22,20 @@ class SetGoalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val fragmentBinding = FragmentSetGoalBinding.inflate(inflater,container,false)
+        if (sharedViewModel.stGoal.value != null) {
+            fragmentBinding.seekBar.setProgress(sharedViewModel.stGoal.value!!/15)
+        } else {
+            fragmentBinding.seekBar.setProgress(0)
+        }
+        fragmentBinding.seekBar.setOnSeekBarChangeListener( object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                fragmentBinding.seekbarValue.text = formatTime(fragmentBinding.seekBar.progress*15)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        fragmentBinding.seekBar.setMax(32)
         binding = fragmentBinding
-        // Inflate the layout for this fragment
-
         return fragmentBinding.root
     }
 
@@ -37,8 +49,19 @@ class SetGoalFragment : Fragment() {
     }
 
     fun saveGoal() {
-        sharedViewModel._stGoal.value = binding!!.setGoalInput.text.toString().toIntOrNull()
+        sharedViewModel._stGoal.value = binding!!.seekBar.progress*15
+        //sharedViewModel._stGoal.value = binding!!.setGoalInput.text.toString().toIntOrNull()
         findNavController().navigate(R.id.action_setGoalFragment_to_todayFragment)
+    }
+
+    fun formatTime(minutes : Int) : String {
+        var formattedTime = ""
+        if (minutes < 60) {
+            formattedTime = getString(R.string.minutes_time_format, minutes)
+        } else {
+            formattedTime = if (minutes.rem(60) > 0)  getString(R.string.hours_time_format, minutes / 60) + " " + getString(R.string.minutes_time_format, minutes.rem(60)) else getString(R.string.hours_time_format, minutes / 60)
+        }
+        return formattedTime
     }
 
 }
--- ScreenTimer/app/src/main/java/com/example/screentimer/ui/TodayFragment.kt
+++ ScreenTimer/app/src/main/java/com/example/screentimer/ui/TodayFragment.kt
@@ -24,28 +24,29 @@ class TodayFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val fragmentBinding = FragmentTodayBinding.inflate(inflater,container, false)
+
         if (UsageStatsService.checkUsageStatsPermission(requireContext())) {
             sharedViewModel._stToday.value = UsageStatsService.getUsageStats(requireContext())
+            if (sharedViewModel.stGoal.value == null) {
+                fragmentBinding.textView.text = getString(R.string.phone_usage_today_nogoal, formatTime(sharedViewModel.stToday.value!!))
+                fragmentBinding.textView1.text = getString(R.string.set_daily_goal_info)
+                fragmentBinding.gauge1.endValue = sharedViewModel.stToday.value!!*2
+                fragmentBinding.gauge1.value = sharedViewModel.stToday.value!!
+            } else if (sharedViewModel.stToday.value!! < sharedViewModel.stGoal.value!!) {
+                fragmentBinding.textView.text = getString(R.string.phone_usage_today, formatTime(sharedViewModel.stToday.value!!), formatTime(sharedViewModel.stGoal.value!!))
+                fragmentBinding.textView1.text = getString(R.string.remaining_screentime, formatTime(sharedViewModel.stGoal.value!! - sharedViewModel.stToday.value!!))
+                fragmentBinding.gauge1.endValue = sharedViewModel.stGoal.value!!
+                fragmentBinding.gauge1.value = sharedViewModel.stToday.value!!
+            } else if (sharedViewModel.stToday.value!! > sharedViewModel.stGoal.value!!) {
+                fragmentBinding.textView.text =  getString(R.string.time_exceeded_info, formatTime(sharedViewModel.stToday.value!!), formatTime(sharedViewModel.stGoal.value!!), formatTime(sharedViewModel.stToday.value!! - sharedViewModel.stGoal.value!!))
+                fragmentBinding.textView1.text = getString(R.string.reassurance)
+                fragmentBinding.gauge1.pointSize = 360
+            }
+            binding = fragmentBinding
         } else {
             startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
-        }
-        val fragmentBinding = FragmentTodayBinding.inflate(inflater,container, false)
-        binding = fragmentBinding
-
-        if (sharedViewModel.stGoal.value == null) {
-            fragmentBinding.textView.text = getString(R.string.phone_usage_today, formatTime(sharedViewModel.stToday.value!!))
-            fragmentBinding.textView1.text = getString(R.string.set_daily_goal_info)
-            fragmentBinding.gauge1.endValue = sharedViewModel.stToday.value!!*2
-            fragmentBinding.gauge1.value = sharedViewModel.stToday.value!!
-        } else if (sharedViewModel.stToday.value!! < sharedViewModel.stGoal.value!!) {
-            fragmentBinding.textView.text = "You have used yout phone for " + sharedViewModel.stToday.value.toString() + " minutes out of "  + sharedViewModel.stGoal.value.toString() + " set as a daily goal."
-            fragmentBinding.textView1.text = "Remaining time until you exceed your daily screentime goal is " + (sharedViewModel.stGoal.value!! - sharedViewModel.stToday.value!!).toString() + " minutes. Use it wisely!"
-            fragmentBinding.gauge1.endValue = sharedViewModel.stGoal.value!!
-            fragmentBinding.gauge1.value = sharedViewModel.stToday.value!!
-        } else if (sharedViewModel.stToday.value!! > sharedViewModel.stGoal.value!!) {
-            fragmentBinding.textView.text = "You have used yout phone for " + sharedViewModel.stToday.value.toString() + " today and exceeded your goal of "  + sharedViewModel.stGoal.value.toString() + " by " + (sharedViewModel.stToday.value!! - sharedViewModel.stGoal.value!!).toString() + " minutes."
-            fragmentBinding.textView1.text = "I hope you do better tomorrow!"
-            fragmentBinding.gauge1.pointSize = 360
         }
         return fragmentBinding.root
     }
@@ -59,7 +60,6 @@ class TodayFragment : Fragment() {
         }
     }
 
-
     fun setGoal() {
         findNavController().navigate(R.id.action_todayFragment_to_setGoalFragment)
     }
@@ -69,7 +69,7 @@ class TodayFragment : Fragment() {
         if (minutes < 60) {
             formattedTime = getString(R.string.minutes_time_format, minutes)
         } else {
-            formattedTime = if (minutes.rem(60) > 0)  getString(R.string.hours_time_format, minutes / 60) + getString(R.string.minutes_time_format, minutes.rem(60)) else getString(R.string.hours_time_format, minutes / 60)
+            formattedTime = if (minutes.rem(60) > 0)  getString(R.string.hours_time_format, minutes / 60) + " " + getString(R.string.minutes_time_format, minutes.rem(60)) else getString(R.string.hours_time_format, minutes / 60)
         }
         return formattedTime
     }
